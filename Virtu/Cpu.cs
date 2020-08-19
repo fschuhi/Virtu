5e7ce8f46c49d291d4acc9d8c08d8ceb05c1f0ae
Frank Schuhardt <frank.schuhardt@gmail.com>
2020-08-19 23:56:18 +0200

    first tentative steps for tracking reads, writes

--- Virtu/Cpu.cs
+++ Virtu/Cpu.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace Jellyfish.Virtu {
 
